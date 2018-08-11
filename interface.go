9c7725994f09c9bd71646938a88d194a0ab8fa5c
daniel robinson <daniel.robinson.open@gmail.com>
2018-08-11 14:20:37 +0100

    update

--- interface.go
+++ interface.go
@@ -11,3 +11,15 @@ type Pool interface {
 type Conn interface{
 	redis.Conn
 }
+
+func NewPool(p *redis.Pool) Pool {
+	return &pool{p}
+}
+
+type pool struct{
+	pool *redis.Pool
+}
+
+func (p *pool) Get() Conn {
+	return p.pool.Get()
+}
